ae8c11a56c54cb33fb34aaf0664743aec985be6c
Hassan <hassansmallah@gmail.com>
2021-11-14 12:43:05 +0300

    save new tasks to DB

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Todo
+
 
 # Create your views here.
 def index(request: HttpResponse):
+    if request.method == 'POST':
+        # for new task, save it to DB
+        new_todo = Todo(
+            title=request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')  # back to home page
     return render(request, 'index.html')
